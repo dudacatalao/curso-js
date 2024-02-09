71ff6b31f562dff27c48945cdc0cfd88e46c48d9
dudacatalao <dmelocatalao@gmail.com>
2024-02-09 16:49:41 -0300

    updates

---
+++ aula 03/script.js
@@ -0,0 +1,15 @@
+// como concatenar anúmeros
+let num1= 10
+let num2 = 2.5
+
+exemple = num1 * num2
+
+console.log(num1.toString() + num2) //temporariamente colocando o numero como uma string para fazer concatenação e não soma
+num1 = num1.toString() //mudando o numero pra string permanente
+console.log(num1.toString(2) )//representação binaria do num1
+console.log(exemple.toFixed(2)) //reduzindo as casas decimais 
+
+// como concatenar anúmeros
+let num3= 10
+let num4 = 2.5
+
